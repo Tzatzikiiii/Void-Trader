9c029db57e65c8189bf5c04b949d3f53531b1c90
FYW <fyw3050@gmail.com>
2022-05-29 16:36:00 +0200

    include splash

---
+++ include/splash.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(splash)
+
+//======================================================================
+//
+//	splash, 240x160@8, 
+//	+ palette 256 entries, not compressed
+//	+ 600 tiles not compressed
+//	Total size: 512 + 38400 = 38912
+//
+//	Time-stamp: 2022-05-29, 15:44:56
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SPLASH_H
+#define GRIT_SPLASH_H
+
+#define splashTilesLen 38400
+extern const unsigned short splashTiles[19200];
+
+#define splashPalLen 512
+extern const unsigned short splashPal[256];
+
+#endif // GRIT_SPLASH_H
+
+//}}BLOCK(splash)
